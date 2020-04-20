41adf736a8c1ce1601afef35e29fc98dd460275b
Henrique Cauê <henrique_caue@hotmail.com>
2020-04-20 03:53:12 -0300

    Página de visualização de clientes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :manufacturers, only: [:index, :show, :new, :create, :edit, :update]
   resources :subsidiaries, only: [:index, :show, :new, :create]
   resources :car_categories, only: [:index, :show, :new, :create]
+  resources :customers, only: [:index, :show]
 end
---
+++ spec/features/user_view_customer_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+feature 'user view customer' do
+  scenario 'succesfully' do
+    Customer.create!(name: 'João', document: 'XXX.XXX.XXX-XX', 
+    email: 'customer@email.com')
+
+    visit root_path
+    click_on 'Clientes'
+
+    expect(page).to have_css('h3', text: 'João')
+  end
+
+  scenario 'and view details' do
+    customer = Customer.create!(name: 'João', document: 'XXX.XXX.XXX-XX', 
+      email: 'customer@email.com')
+
+    visit root_path
+    click_on 'Clientes'
+    click_on customer.name
+
+    expect(page).to have_css('h1', text: customer.name)
+    expect(page).to have_css('p', text: customer.document)
+    expect(page).to have_css('p', text: customer.email)
+  end   
+
+  scenario 'and no customers are created' do
+    visit root_path
+    click_on 'Clientes'
+
+    expect(page).to have_css('h1', text: 'Não há clientes cadastrados')
+
+  end   
+
+  scenario 'and find return button' do
+    visit root_path
+    click_on 'Clientes'
+
+    expect(page).to have_link('Voltar', href: root_path)
+
+  end
+
+  scenario 'and find return button' do
+    customer = Customer.create!(name: 'João', document: 'XXX.XXX.XXX-XX', 
+    email: 'customer@email.com')
+      
+    visit root_path
+    click_on 'Clientes'
+    click_on customer.name
+
+    expect(page).to have_link('Voltar', href: customers_path)
+
+  end
+
+
+  scenario 'and return to home page' do
+    visit root_path
+    click_on 'Clientes'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+
+  end
+
+  scenario 'and return to customers page' do
+    customer = Customer.create!(name: 'João', document: 'XXX.XXX.XXX-XX', 
+    email: 'customer@email.com')
+    
+    visit root_path
+    click_on 'Clientes'
+    click_on customer.name
+    click_on 'Voltar'
+
+    expect(current_path).to eq customers_path
+
+  end
+end
