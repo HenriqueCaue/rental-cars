0790acd70265f60af23c4f980005027c2e362dbc
Henrique Cauê <henrique_caue@hotmail.com>
2020-04-22 01:52:13 -0300

    Cadatro de cliente

---
+++ spec/features/user_register_customer_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'User register customer' do
+  scenario 'from index page' do
+    visit root_path
+    click_on 'Clientes'
+
+    expect(page).to have_link('Registrar novo cliente', href: new_customer_path)
+  end
+
+  scenario 'and view form' do
+    visit root_path
+    click_on 'Clientes'
+    click_on 'Registrar novo cliente'
+
+    expect(page).to have_content('Nome')
+    expect(page).to have_content('CPF')
+    expect(page).to have_content('Endereço de email')
+  end
+
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Clientes'
+    click_on 'Registrar novo cliente'
+
+    fill_in 'Nome', with: 'João'
+    fill_in 'CPF', with: 'XXX.XXX.XXX-XX'
+    fill_in 'Endereço de email', with: 'customer@gmail.com'
+    click_on 'Enviar'
+
+    expect(current_path).to eq customer_path(Customer.last.id)
+    expect(page).to have_css('h1', text: 'João')
+    expect(page).to have_css('p', text: 'XXX.XXX.XXX-XX')
+    expect(page).to have_css('p', text: 'customer@gmail.com')
+
+    expect(page).to have_link('Voltar')
+  end
+end
