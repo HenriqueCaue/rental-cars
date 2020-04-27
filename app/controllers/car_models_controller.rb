f0864d01f4307ce5628aeb03090e288a7ce34c52
Henrique Cauê <henrique_caue@hotmail.com>
2020-04-27 15:32:39 -0300

    Cadastro de modelos de carros

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -6,4 +6,24 @@ class CarModelsController < ApplicationController
   def show
     @car_model = CarModel.find(params[:id])
   end
+
+  def new
+    @car_model = CarModel.new
+    @manufacturer = Manufacturer.all
+    @car_category = CarCategory.all
+  end
+
+  def create
+    #@car_model = CarModel.create(car_model_params) sem precisar do save
+    @car_model = CarModel.new(car_model_params)
+    @car_model.save
+    redirect_to @car_model
+  end
+
+  private
+
+  def car_model_params
+    params.require(:car_model).permit(:name, :motorization, 
+      :fuel_type, :year, :manufacturer_id, :car_category_id)
+  end
 end
