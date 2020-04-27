b10fb29ad46d6c029b140f033e6290f778c04f20
Henrique Cauê <henrique_caue@hotmail.com>
2020-04-27 17:27:35 -0300

    Registro de modelos de carro e I18n

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -14,10 +14,15 @@ class CarModelsController < ApplicationController
   end
 
   def create
-    #@car_model = CarModel.create(car_model_params) sem precisar do save
+    #@car_model = CarModel.create!(car_model_params) sem precisar do save
     @car_model = CarModel.new(car_model_params)
-    @car_model.save
-    redirect_to @car_model
+    if @car_model.save
+      redirect_to @car_model
+    else
+      @manufacturer = Manufacturer.all
+      @car_category = CarCategory.all
+      render :new
+    end
   end
 
   private
--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,4 +1,7 @@
 class CarModel < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :car_category
+  
+  validates :name, :year, :motorization, :fuel_type, :manufacturer_id, :car_category_id, presence: true
+
 end
