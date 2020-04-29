2916ce80f70d1355c93483c9bfab641744cb08a0
Henrique Cauê <henrique_caue@hotmail.com>
2020-04-28 23:54:50 -0300

    Pssobilidade de cadastrar locações de carros

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,23 @@
+class RentalsController < ApplicationController
+  def index
+    @rentals = Rental.all
+  end
+
+  def new
+    @rental = Rental.new
+    @customer = Customer.all
+    @car_category = CarCategory.all
+  end
+
+  def create
+    @rental = Rental.create(rental_params)
+    redirect_to rentals_path
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, 
+                                    :customer_id, :car_category_id)
+  end
+end
